0bf105df9afecb7b2894b071821b78b175fe99dc
Viviane Dias <vivianedias@pm.me>
2023-08-17 11:04:07 -0300

    feat: responds with json on api

--- api/index.py
+++ api/index.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, json
+
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    data = "hello WORLD"
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
