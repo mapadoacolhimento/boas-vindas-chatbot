2f414a6d8f040163f32e36db0ccd11b32ab995d5
Viviane Dias <vivianedias@pm.me>
2023-08-17 11:37:36 -0300

    feat(api): chat endpoint accepts post and returns hardcoded msg

--- api/index.py
+++ api/index.py
@@ -2,11 +2,10 @@ from flask import Flask, json
 
 app = Flask(__name__)
 
-@app.route("/api/python")
+@app.route("/api/chat", methods = ['POST'])
 def hello_world():
-    data = "hello WORLD"
     response = app.response_class(
-        response=json.dumps(data),
+        response="olá from python :)",
         status=200,
         mimetype='application/json'
     )
