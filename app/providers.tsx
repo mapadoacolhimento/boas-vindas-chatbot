41e5f8aabfa1dee504d9ee794cb64090e6b2c963
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-08-23 19:53:06 -0300

    adding chatgpt layout specs using chakra

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, theme} from '@chakra-ui/react'
 
 export function Providers({ 
     children 
