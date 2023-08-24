459c4399682e1a2229c96e88d5d9254f00bc1204
Viviane Dias <vivianedias@pm.me>
2023-08-24 17:13:29 -0300

    feat: refactor page styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,45 @@
 "use client";
 
-import { Page } from "@vercel/examples-ui";
 import { Chat } from "../components";
-import { Stack, Text, Box, Center, Flex } from "@chakra-ui/react";
+import { Text, Box, Flex, VStack, Heading } from "@chakra-ui/react";
 import Image from "next/image";
 
-
- 
 function Home() {
   return (
-    <Page>
-      <Stack direction={["column"]} spacing="10em">
-        <Box w="100%" h="32px">
-          <section>
-            <Center fontSize="4xl" bg="white" h="100px" color="black">
-              Exemplo de chat
-            </Center>
-          </section>
-        </Box>
+    <VStack spacing="10em" justify={"center"}>
+      <Heading>Exemplo de chat</Heading>
+      <Flex w={"full"} justify={"center"}>
         <Box
-          w="54.5rem"
-          h="22.5625rem"
+          w={{ base: "full", lg: "80%" }}
+          minH="22.5625rem"
           bg="#FFF"
           border="1px solid #C4C4C4"
           borderRadius="0.625rem"
         >
-          <section>
-            <Flex
-              pb="0.72rem"
-              pt="1.31rem"
-              pl="1.56rem"
-              borderBottom="1px solid #EFEFEF"
-            >
-              <Box p="6px" borderRadius="3px" bg="#FFDBDB">
-                <Image
-                  src="/icons/icon-good-msg.svg"
-                  alt="Icon de mensagem"
-                  height={13.69}
-                  width={13.69}
-                />
-              </Box>
-              <Box>
-                <Text variant="h2">Fale com a IAna</Text>
-              </Box>
-            </Flex>
-            <div>
-              <Chat />
-            </div>
-          </section>
+          <Flex
+            pb="0.72rem"
+            pt="1.31rem"
+            pl="1.56rem"
+            borderBottom="1px solid #EFEFEF"
+          >
+            <Box p="6px" borderRadius="3px" bg="#FFDBDB">
+              <Image
+                src="/icons/icon-good-msg.svg"
+                alt="Icon de mensagem"
+                height={13.69}
+                width={13.69}
+              />
+            </Box>
+            <Box>
+              <Text variant="h2">Fale com a IAna</Text>
+            </Box>
+          </Flex>
+          <Box>
+            <Chat />
+          </Box>
         </Box>
-      </Stack>
-    </Page>
+      </Flex>
+    </VStack>
   );
 }
 
