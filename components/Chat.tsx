e76672bf27c7162cb2e3376a4417168d2ff16b94
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-08-28 12:34:47 -0300

    Atualizando a cor da boxShadow de RGBA para HEX

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -23,13 +23,13 @@ const InputMessage = ({ input, setInput, sendMessage }: any) => (
           borderWidth:"1px",
           borderStyle:"solid",
           borderColor:"brand.magenta",
-          boxShadow:"0px 3px 10px 0px rgba(0, 0, 0, 0.10)"
+          boxShadow:"0px 3px 10px 0px #0000001a"
         }}
         _focus={{
           borderWidth:"1px",
           borderStyle:"solid",
           borderColor:"brand.magenta",
-          boxShadow:"0px 3px 10px 0px rgba(0, 0, 0, 0.10)"
+          boxShadow:"0px 3px 10px 0px #0000001a"
         }}
         placeholder='Envie sua pergunta ou dúvida e a aIAna vai te ajudar'
         _placeholder={{
