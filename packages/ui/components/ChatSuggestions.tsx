147f14bfc5e9bd5a545b305c311473e1d2b1b316
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-08-30 12:03:09 -0300

    Adicionei um Flex row para botões, adicionei o componente Button e a estilização dos botões

--- packages/ui/components/ChatSuggestions.tsx
+++ packages/ui/components/ChatSuggestions.tsx
@@ -1,14 +1,31 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, Flex, Button } from "@chakra-ui/react"
 
 const ChatSuggestions = ({}: any) => (
   <Box
     pl="1.5rem"
-    pb="3rem"
+    pb="1rem"
     fontSize="1rem"
     fontWeight="600"
     color="brand.default"
   >
     <Text>Não sabe como começar? Temos algumas sugestões de conteúdo:</Text>
+    <Flex
+      direction="row"
+      pt="1rem"
+    >
+      <Button
+        p="0.312 0.437 0.37 0.437"
+        border="1px solid #E0E0E0"
+        borderRadius="0.5rem"
+        color="brand.darkGray"
+        bg="#FFFFF"
+        fontSize="0.687rem"
+        _hover={{
+          bg:"#EFEFEF"
+        }}
+      >
+        Tipos de serviço público</Button>
+    </Flex>
   </Box>
 )
 
