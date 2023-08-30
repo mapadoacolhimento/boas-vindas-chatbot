e84a7b7bc1724adc6a362e9b5decd6d8b425be94
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-08-30 10:48:44 -0300

    Criando componente ChatSuggestions e adicionando o Box de texto no componente

---
+++ packages/ui/components/ChatSuggestions.tsx
@@ -0,0 +1,15 @@
+import { Box, Text } from "@chakra-ui/react"
+
+const ChatSuggestions = ({}: any) => (
+  <Box
+    pl="1.5rem"
+    pb="3rem"
+    fontSize="1rem"
+    fontWeight="600"
+    color="brand.default"
+  >
+    <Text>Não sabe como começar? Temos algumas sugestões de conteúdo:</Text>
+  </Box>
+)
+
+export default ChatSuggestions;
