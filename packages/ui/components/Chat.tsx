4897c7ead6efba845d466ed83dda47247c506a94
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-09-01 16:13:11 -0300

    Adicionando componente de botão mobile, fiz a implementação de estilo especificado no layout

--- packages/ui/components/Chat.tsx
+++ packages/ui/components/Chat.tsx
@@ -2,10 +2,11 @@
 
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import { Button, Input, HStack, Box, VStack } from "@chakra-ui/react";
+import { Button, Input, HStack, Box, VStack, InputGroup, InputRightElement } from "@chakra-ui/react";
 
 import ChatLine, { type ChatGPTMessage, LoadingChatLine } from "./ChatLine";
 import ChatSuggestions from "@/components/ChatSuggestions";
+import SendMessageIcon from "@/components/SendMessageIcon";
 
 const COOKIE_NAME = "nextjs-example-ai-chat-gpt3";
 
@@ -18,66 +19,110 @@ export const initialMessages: ChatGPTMessage[] = [
 ];
 
 const InputMessage = ({ input, setInput, sendMessage }: any) => (
-  <HStack spacing={4} px={6} w={"full"}>
-    <Box flex="1">
-      <Input
-        _hover={{
-          borderWidth: "1px",
-          borderStyle: "solid",
-          borderColor: "brand.mediumPurple",
-          boxShadow: "0px 3px 10px 0px #0000001a",
-        }}
-        _focus={{
-          borderWidth: "1px",
-          borderStyle: "solid",
-          borderColor: "brand.mediumPurple",
-          boxShadow: "0px 3px 10px 0px #0000001a",
-        }}
-        placeholder="Envie sua pergunta ou dúvida e a aIAna vai te ajudar"
-        _placeholder={{
-          opacity: 1,
-          color: "#AAA",
-          size: "md",
-          fontsize: "13px",
-          fontWeight: "400",
-        }}
-        borderRadius="7px"
-        border="1px solid"
-        borderColor="brand.lightGray"
-        type="text"
-        aria-label="chat input"
-        required
-        value={input}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
+  <>
+    <HStack
+      display={{ base: "none", md: "flex" }}
+      spacing={4} px={6} w={"full"}
+    >
+        <Box flex="1">
+          <Input
+            _hover={{
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderColor: "brand.mediumPurple",
+              boxShadow: "0px 3px 10px 0px #0000001a",
+            }}
+            _focus={{
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderColor: "brand.mediumPurple",
+              boxShadow: "0px 3px 10px 0px #0000001a",
+            }}
+            placeholder="Envie sua pergunta ou dúvida e a aIAna vai te ajudar"
+            _placeholder={{
+              opacity: 1,
+              color: "#AAA",
+              size: "md",
+              fontsize: "13px",
+              fontWeight: "400",
+            }}
+            borderRadius="7px"
+            border="1px solid"
+            borderColor="brand.lightGray"
+            type="text"
+            aria-label="chat input"
+            required
+            value={input}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage(input);
+                setInput("");
+              }
+            }}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+        </Box>
+        <Button
+          textTransform={"uppercase"}
+          bg="brand.mainGray"
+          color="white"
+          _hover={{
+            bg: "brand.light",
+          }}
+          _active={{
+            bg: "brand.dark",
+          }}
+          type="submit"
+          onClick={() => {
             sendMessage(input);
             setInput("");
-          }
-        }}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-      />
-    </Box>
-    <Button
-      textTransform={"uppercase"}
-      bg="brand.mainGray"
-      color="white"
-      _hover={{
-        bg: "brand.light",
-      }}
-      _active={{
-        bg: "brand.dark",
-      }}
-      type="submit"
-      onClick={() => {
-        sendMessage(input);
-        setInput("");
-      }}
-    >
-      Enviar
-    </Button>
-  </HStack>
+          }}
+        >
+          Enviar
+        </Button>
+    </HStack>
+    <HStack
+        display={{base: "flex", md: "none"}}
+        spacing={4} px={6} w={"full"}>
+        <InputGroup size='lg'>
+          <Input
+            type={'text'}
+            aria-label="input chat"
+            placeholder="Envie sua pergunta ou dúvida"
+            _placeholder={{
+              opacity: 1,
+              color: "brand.mainGray",
+              size: "md",
+              fontsize: "13px",
+              fontWeight: "400",
+            }}
+            borderRadius="7px"
+            border="1px solid"
+            borderColor="brand.lightGray"
+            _hover={{
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderColor: "brand.mediumPurple",
+              boxShadow: "0px 3px 10px 0px #0000001a",
+            }}
+            _focus={{
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderColor: "brand.mediumPurple",
+              boxShadow: "0px 3px 10px 0px #0000001a",
+            }}
+          />
+          <InputRightElement px={"16px"} py={"16px"}>
+            <Button size='lg' bg="transparent">
+              <SendMessageIcon />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </HStack>
+  </>
+  
 );
 
 export default function Chat() {
