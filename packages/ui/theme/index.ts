b26dfeb1468df7f5c0e86788f4aa928b666c39fb
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-09-04 11:15:42 -0300

    Adicionando mediumPurple ao brand de cores

--- packages/ui/theme/index.ts
+++ packages/ui/theme/index.ts
@@ -39,6 +39,7 @@ const theme = extendTheme({
       brand: {
         primary: "#622565",
         primaryLight: "#732E76",
+        mediumPurple: "#C68CB9",
         primaryDark: "#57215A",
         secondary: "#E8B638",
         primaryGray: "#AAAAAA",
