3f6818444e646d83c0e9dd77f9aab77218de1136
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-09-08 14:46:20 -0300

    Importei o arquivo css, removi as estilizações tailwind e implementei o css com o loading do chat

--- packages/ui/components/ChatLine.tsx
+++ packages/ui/components/ChatLine.tsx
@@ -2,6 +2,8 @@ import { Box, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 import UserQueryIcon from "./UserQueryIcon";
 import ChatIcon from "./ChatIcon";
 
+import styles from "../styles/loadingState.module.css";
+
 // wrap Balancer to remove type errors :( - @TODO - fix this ugly hack
 // const BalancerWrapper = (props: any) => <Balancer {...props} />
 
@@ -14,24 +16,14 @@ export interface ChatGPTMessage {
 
 // loading placeholder animation for the chat line
 export const LoadingChatLine = () => (
-  <div className="flex min-w-full animate-pulse px-4 py-5 sm:px-6">
-    <div className="flex flex-grow space-x-3">
-      <div className="min-w-0 flex-1">
-        <p className="font-large text-xxl text-gray-900">
-          <a href="#" className="hover:underline">
-            AI
-          </a>
-        </p>
-        <div className="space-y-4 pt-4">
-          <div className="grid grid-cols-3 gap-4">
-            <div className="col-span-2 h-2 rounded bg-zinc-500"></div>
-            <div className="col-span-1 h-2 rounded bg-zinc-500"></div>
-          </div>
-          <div className="h-2 rounded bg-zinc-500"></div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Box className={styles.container}>
+    <Box className={styles.tiblock}>
+      <Box className={styles.tidot}></Box>
+      <Box className={styles.tidot}></Box>
+      <Box className={styles.tidot}></Box>
+    </Box>
+    
+  </Box>
 );
 
 // util helper to convert new lines to <br /> tags
