79a85961bd3b3b57b31c21c0bda5c7671162db34
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-09-08 15:01:49 -0300

    Arrumando indentação

--- packages/ui/components/ChatLine.tsx
+++ packages/ui/components/ChatLine.tsx
@@ -21,8 +21,7 @@ export const LoadingChatLine = () => (
       <Box className={styles.tidot}></Box>
       <Box className={styles.tidot}></Box>
       <Box className={styles.tidot}></Box>
-    </Box>
-    
+    </Box> 
   </Box>
 );
 
