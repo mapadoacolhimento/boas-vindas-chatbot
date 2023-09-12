a8148fb449773823c77499317896d88ef3734aa5
Viviane Dias <vivianedias@pm.me>
2023-09-12 20:35:12 -0300

    chore: configure correct ports and scripts to run api-v2 locally with ui

--- packages/ui/next.config.js
+++ packages/ui/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const REST_API = process.env.NEXT_PUBLIC_AWS_REST_API || "http://localhost:3001"
+const REST_API = process.env.NEXT_PUBLIC_AWS_REST_API || "http://localhost:5000/dev"
 const nextConfig = {
   async rewrites() {
     return [
