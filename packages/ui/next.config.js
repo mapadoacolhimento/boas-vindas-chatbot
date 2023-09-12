9b0da1d9ef799c6d210d44242bc0ee82360e2d96
Viviane Dias <vivianedias@pm.me>
2023-09-12 20:17:57 -0300

    fix(ui): adds fallback url when no rest api is provided

--- packages/ui/next.config.js
+++ packages/ui/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const REST_API = process.env.NEXT_PUBLIC_AWS_REST_API || "http://localhost:3001"
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/chat",
-        destination: `${process.env.NEXT_PUBLIC_AWS_REST_API}/chat`,
+        destination: `${REST_API}/chat`,
       },
     ];
   },
