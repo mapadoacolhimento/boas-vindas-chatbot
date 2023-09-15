a6da23ccd89042f4d9960c0b855dde24f59e955a
root <oliveirqangela96@gmail.com>
2023-09-15 15:42:50 -0300

    refatorando input mobile adicionando o textarea e adicionando responsividade ao input moblie e desktop

--- packages/ui/components/InputMessage.tsx
+++ packages/ui/components/InputMessage.tsx
@@ -44,7 +44,48 @@ const InputMessage = ({
   return (
     <Box px={6} w={"full"}>
       <InputGroup gap={{ base: 0, md: 4 }}>
+        <Input
+          display={{ base: "none", md: "initial" }}
+          _hover={{
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: "brand.primaryMedium",
+            boxShadow: "0px 3px 10px 0px #0000001a",
+          }}
+          _focus={{
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: "brand.primaryMedium",
+            boxShadow: "0px 3px 10px 0px #0000001a",
+          }}
+          placeholder={placeholder}
+          _placeholder={{
+            opacity: 1,
+            color: "placeholder",
+            size: "md",
+            fontsize: "13px",
+            fontWeight: "400",
+          }}
+          borderRadius="md"
+          border="1px solid"
+          borderColor="brand.lightGray"
+          type="text"
+          aria-label="Campo de envio de mensagem"
+          required
+          value={input}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              sendMessage(input);
+              setInput("");
+            }
+          }}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+
         <AutoResizeTextarea
+          display={{ base: "initial", md: "none" }}
           _hover={{
             borderWidth: "1px",
             borderStyle: "solid",
@@ -87,71 +128,9 @@ const InputMessage = ({
           pr={35}
         />
         <InputRightElement
+          display={{ base: "initial", md: "none" }}
           h="100%"
         >
-          <IconButton
-            size={"xs"}
-            color={"white"}
-            icon={<SendMessageIcon boxSize={4} />}
-            aria-label="Enviar mensagem"
-            type="submit"
-            onClick={() => {
-              sendMessage(input);
-              setInput("");
-            }}
-            isDisabled={input === ""}
-            _disabled={{
-              bgColor: "transparent",
-              color: "brand.primaryGray",
-            }}
-          />
-        </InputRightElement>
-      </InputGroup>
-    </Box>
-  );
-  return (
-    <Box px={6} w={"full"}>
-      <InputGroup gap={{ base: 0, md: 4 }}>
-        <Input
-          _hover={{
-            borderWidth: "1px",
-            borderStyle: "solid",
-            borderColor: "brand.primaryMedium",
-            boxShadow: "0px 3px 10px 0px #0000001a",
-          }}
-          _focus={{
-            borderWidth: "1px",
-            borderStyle: "solid",
-            borderColor: "brand.primaryMedium",
-            boxShadow: "0px 3px 10px 0px #0000001a",
-          }}
-          placeholder={placeholder}
-          _placeholder={{
-            opacity: 1,
-            color: "placeholder",
-            size: "md",
-            fontsize: "13px",
-            fontWeight: "400",
-          }}
-          borderRadius="md"
-          border="1px solid"
-          borderColor="brand.lightGray"
-          type="text"
-          aria-label="Campo de envio de mensagem"
-          required
-          value={input}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              sendMessage(input);
-              setInput("");
-            }
-          }}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-        />
-
-        <InputRightElement display={{ base: "flex", md: "none" }}>
           <IconButton
             size={"xs"}
             color={"white"}
