06b02bcf95b09403c76d425a19926f0b0e602d2e
root <oliveirqangela96@gmail.com>
2023-09-15 15:25:53 -0300

    Fiz a instalação do autozise e implementação do textarea no input

--- packages/ui/components/InputMessage.tsx
+++ packages/ui/components/InputMessage.tsx
@@ -6,8 +6,26 @@ import {
   InputGroup,
   InputRightElement,
   useMediaQuery,
+  Textarea,
+  forwardRef
 } from "@chakra-ui/react";
+import ResizeTextarea from "react-textarea-autosize";
+import { SearchIcon } from "@chakra-ui/icons";
+
 import SendMessageIcon from "./SendMessageIcon";
+
+export const AutoResizeTextarea = forwardRef((props, ref) => (
+  <Textarea
+    minH="unset"
+    overflow="hidden"
+    w="100%"
+    resize="none"
+    ref={ref}
+    minRows={3}
+    as={ResizeTextarea}
+    {...props}
+  />
+));
 
 const InputMessage = ({
   input,
@@ -23,6 +41,74 @@ const InputMessage = ({
     ? "Envie sua pergunta ou dúvida e a IAna vai te ajudar"
     : "Envie sua pergunta ou dúvida";
 
+  return (
+    <Box px={6} w={"full"}>
+      <InputGroup gap={{ base: 0, md: 4 }}>
+        <AutoResizeTextarea
+          _hover={{
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: "brand.primaryMedium",
+            boxShadow: "0px 3px 10px 0px #0000001a",
+          }}
+          _focus={{
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: "brand.primaryMedium",
+            boxShadow: "0px 3px 10px 0px #0000001a",
+          }}
+          placeholder={placeholder}
+          _placeholder={{
+            opacity: 1,
+            color: "placeholder",
+            size: "md",
+            fontsize: "13px",
+            fontWeight: "400",
+          }}
+          borderRadius="md"
+          border="1px solid"
+          borderColor="brand.lightGray"
+          aria-label="Campo de envio de mensagem"
+          required
+          value={input}
+          onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === "Enter") {
+              sendMessage(input);
+              setInput("");
+            }
+          }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setInput(event.target.value);
+          }}
+
+          variant="outline"
+          size="xs"
+          minRows={1}
+          pr={35}
+        />
+        <InputRightElement
+          h="100%"
+        >
+          <IconButton
+            size={"xs"}
+            color={"white"}
+            icon={<SendMessageIcon boxSize={4} />}
+            aria-label="Enviar mensagem"
+            type="submit"
+            onClick={() => {
+              sendMessage(input);
+              setInput("");
+            }}
+            isDisabled={input === ""}
+            _disabled={{
+              bgColor: "transparent",
+              color: "brand.primaryGray",
+            }}
+          />
+        </InputRightElement>
+      </InputGroup>
+    </Box>
+  );
   return (
     <Box px={6} w={"full"}>
       <InputGroup gap={{ base: 0, md: 4 }}>
