6cab085c14d3b76d7d953b40fe6c8477fd6b7f68
Camila Cardoso <caamila.cardoso.l@gmail.com>
2023-09-22 18:32:55 -0300

    feat: insere três perguntas de avaliacao e melhora a página

--- packages/ui/app/page.tsx
+++ packages/ui/app/page.tsx
@@ -116,6 +116,7 @@ function Home() {
           sendMessage={sendMessage}
           messages={messages}
           setMessages={setMessages}
+          showSuggestions={true}
         />
       </VStack>
     </VStack>
