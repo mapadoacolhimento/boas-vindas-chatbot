e4c0348adbb0748b5687c15b7f5bae8f663d8b61
Viviane Dias <vivi@mapa.org.br>
2024-02-05 17:58:41 -0300

    fix: converts user_id to number before sending to hasura

--- packages/ui/pages/api/feedback.ts
+++ packages/ui/pages/api/feedback.ts
@@ -1,8 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const FIRST_QUESTION = "Qual foi sua percepção ao interagir com a IAna?";
+export const FIRST_QUESTION =
+  'Qual foi sua percepção ao interagir com a IAna?';
 export const SECOND_QUESTION =
-  "Em uma escala de 0 a 5, que nota você daria à sua experiência com a IAna? Por favor, use apenas números.";
+  'Em uma escala de 0 a 5, que nota você daria à sua experiência com a IAna? Por favor, use apenas números.';
 
 const MUTATION = `mutation mapa_do_acolhimento_iana_feedback($answers: [mapa_do_acolhimento_iana_feedback_insert_input!]!) {
   insert_mapa_do_acolhimento_iana_feedback(objects: $answers) {
@@ -24,12 +25,20 @@ export default async function handler(
     const { firstAnswer, rating, userId } = req.body;
 
     if (!firstAnswer || !rating || !userId) {
-      throw new Error("Invalid req body");
+      throw new Error('Invalid req body');
     }
 
     const answers = [
-      { answer: firstAnswer, question: FIRST_QUESTION, user_id: userId },
-      { answer: `${rating}`, question: SECOND_QUESTION, user_id: userId },
+      {
+        answer: firstAnswer,
+        question: FIRST_QUESTION,
+        user_id: Number(userId),
+      },
+      {
+        answer: `${rating}`,
+        question: SECOND_QUESTION,
+        user_id: Number(userId),
+      },
     ];
     const graphqlQuery = {
       query: MUTATION,
@@ -38,19 +47,24 @@ export default async function handler(
       },
     };
 
-    const graphqlApiRes = await fetch(`${process.env.GRAPHQL_HTTP_URL}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-hasura-admin-secret": `${process.env.HASURA_SECRET}`,
-      },
-      body: JSON.stringify(graphqlQuery),
-    });
+    const graphqlApiRes = await fetch(
+      `${process.env.GRAPHQL_HTTP_URL}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-hasura-admin-secret': `${process.env.HASURA_SECRET}`,
+        },
+        body: JSON.stringify(graphqlQuery),
+      }
+    );
 
     const data = await graphqlApiRes.json();
 
     if (data && data.errors && data.errors.length > 0) {
-      throw new Error(data.errors.map((e: any) => e.message).join(" "));
+      throw new Error(
+        data.errors.map((e: any) => e.message).join(' ')
+      );
     }
 
     return res.status(200).json(data);
