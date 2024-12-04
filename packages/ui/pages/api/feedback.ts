74c415c03fe05cd2cffaf21889551235eb24e6bc
Viviane Dias <vivi@mapa.org.br>
2024-12-04 15:35:47 -0300

    feat: dont use snake case when declaring const

--- packages/ui/pages/api/feedback.ts
+++ packages/ui/pages/api/feedback.ts
@@ -1,48 +1,45 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import  prisma  from './db';
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./db";
 
-export const FIRST_QUESTION =
-  'Qual foi sua percepção ao interagir com a IAna?';
+export const FIRST_QUESTION = "Qual foi sua percepção ao interagir com a IAna?";
 export const SECOND_QUESTION =
-  'Em uma escala de 0 a 5, que nota você daria à sua experiência com a IAna? Por favor, use apenas números.';
-
+	"Em uma escala de 0 a 5, que nota você daria à sua experiência com a IAna? Por favor, use apenas números.";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
+	req: NextApiRequest,
+	res: NextApiResponse<any>
 ) {
-  try {
-    const { firstAnswer, rating, userId } = req.body;
+	try {
+		const { firstAnswer, rating, userId } = req.body;
 
-    if (!firstAnswer || !rating || !userId) {
-      throw new Error('Invalid req body');
-    }
+		if (!firstAnswer || !rating || !userId) {
+			throw new Error("Invalid req body");
+		}
 
-    const answers = [
-      {
-        answer: firstAnswer,
-        question: FIRST_QUESTION,
-        user_id: Number(userId),
-      },
-      {
-        answer: `${rating}`,
-        question: SECOND_QUESTION,
-        user_id: Number(userId),
-      },
-    ];
-   
-    const iana_feedbacks = await prisma.iana_feedback.createMany( {
-      data : answers
-      
-    }); 
+		const answers = [
+			{
+				answer: firstAnswer,
+				question: FIRST_QUESTION,
+				user_id: Number(userId),
+			},
+			{
+				answer: `${rating}`,
+				question: SECOND_QUESTION,
+				user_id: Number(userId),
+			},
+		];
 
-    return res.status(200).json(iana_feedbacks);
-  } catch (e: any) {
-    console.error(e);
-    return res
-      .status(500)
-      .send(
-        `Something went wrong when saving the feedback answer: ${e.message}`
-      );
-  }
+		const feedbacks = await prisma.iana_feedback.createMany({
+			data: answers,
+		});
+
+		return res.status(200).json(feedbacks);
+	} catch (e: any) {
+		console.error(e);
+		return res
+			.status(500)
+			.send(
+				`Something went wrong when saving the feedback answer: ${e.message}`
+			);
+	}
 }
